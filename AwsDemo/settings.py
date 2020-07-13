9a7c6c0a9ba5fe375c81f2c3587ecda262ac259f
Rajendran Girish Pillai <rgirishpillai77@gmail.com>
2020-07-13 08:04:13 +0530

    commit message

--- AwsDemo/settings.py
+++ AwsDemo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7ej*v925j_y%^1j7q_nvtcdl16iv2!b7ip74bz^69f#q1(m5ml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
